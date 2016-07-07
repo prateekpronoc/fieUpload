7222b85aafd6cdb9958bb3254ca9ac3b3edf1dbf
prateeks <prateek.s@hirepro.in>
2016-07-07 12:43:10 +0530

    initial setup

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('fieUpload')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('fieUpload', ['ngResource', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
